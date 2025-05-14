027e7937a5e9dc7d52909e4beb2dc94cbc18de39
dnttr <147146363+dnttr@users.noreply.github.com>
2025-05-14 23:53:53 +0200

    Initial Commit

---
+++ libcoregraphics/libcoregraphics.h
@@ -0,0 +1,12 @@
+//
+//  libcoregraphics.h
+//  libcoregraphics
+//
+//  Created by Damian Netter on 14/05/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libcoregraphics : NSObject
+
+@end
---
+++ libcoregraphics/libcoregraphics.m
@@ -0,0 +1,12 @@
+//
+//  libcoregraphics.m
+//  libcoregraphics
+//
+//  Created by Damian Netter on 14/05/2025.
+//
+
+#import "libcoregraphics.h"
+
+@implementation libcoregraphics
+
+@end
